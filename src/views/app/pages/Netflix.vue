f31a41d9db0e50e02d88adab414893200c15e48a
Ajaisvraj <ajaisvraj7907@gmail.com>
2023-08-06 22:28:28 +0530

    finished netflix

--- src/views/app/pages/Netflix.vue
+++ src/views/app/pages/Netflix.vue
@@ -40,8 +40,107 @@
                         <div class="mobile-logo"><img class="mblogo" src="/netflix/netflix_mobile.jpg" alt="mobile"></div>
 
                     </div>
+                </div>
+                <div class="fourth_div_main">
+                    <div class="fourth_div">
+                        <div class="watch_head"><h1 class="watchevr">Watch everywhere</h1></div>
+                        <div><h2 class="subhead-3">Stream unlimited movies and TV shows on your <br> phone, tablet, laptop, and TV.</h2></div>
+                        <div class="all_in_one"><img class="all-inone" src="/netflix/netflix-all.png" alt=""></div>
+                    </div>
+                </div>
+
+                <div class="fifth_div_main">
+                    <div class="fifth_div">
+                        <div class="kids_head"><h1 class="forkids">Create profiles for kids</h1></div>
+                        <div><h2 class="subhead-4">Send children on adventures with their favourite <br> characters in a space made just for them—free with <br> your membership.</h2></div>
+                        <div><img class="children" src="/netflix/netflix-children.png" alt=""></div>
+                    </div>
+                </div>
+
+                <div class="sixth_div_main">
+                    <div class="fifth_div">
+                        <div class="quest_head"><h1 class="questions">Frequently Asked Questions</h1></div>
+                        <div class="ques-1"><h1 class="subhead-5">What is Netflix ?</h1>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div class="sevnth_div_main">
+                    <div class="sevnth_div">
+                        <div class="ques-2"><h1 class="subhead-6">How much does Netflix cost ?</h1> 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="eight_div_main">
+                    <div class="eight_div">
+                        <div class="ques-3"><h1 class="subhead-7">Where can i watch ?</h1> 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="nineth_div_main">
+                    <div class="ninth_div">
+                        <div class="ques-4"><h1 class="subhead-8">How do i cancel ?</h1> 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="tenth_div_main">
+                    <div class="tenth_div">
+                        <div class="ques-5"><h1 class="subhead-9">What can i watch on Netflix ?</h1> 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="eleventh_div_main">
+                    <div class="eleventh_div">
+                        <div class="ques-6"><h1 class="subhead-10">Is Netflix good for kids ?</h1> 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="emailbox">
+                    <div class="getstarted"><h3 class="forh3">Ready to watch? Enter your email to create or restart your membership.</h3></div>
+                </div>
+                <div class="email2"><input class="getaddress" type="text" placeholder="Email address"></div>
+
+                <div class="btnget">
+                    <div><button class="getbtn">Get Started ⨠</button></div>
+                </div>
+
+                <div class="footer_div_main">
+                    <div class="footer_div">
+                        <footer class="footer">
+    Questions? Call 000-800-919-1694
+    <br>
+    <a href="#">FAQ</a>
+    <a href="#">Help Centre</a>
+    <a href="#">Account</a>
+    <a href="#">Media Centre</a>
+    <a href="#">Investor Relations</a>
+    <a href="#">Jobs</a>
+    <a href="#">Ways to Watch</a>
+    <a href="#">Terms of Use</a>
+    <a href="#">Privacy</a>
+    <a href="#">Cookie Preferences</a>
+    <a href="#">Corporate Information</a>
+    <a href="#">Contact Us</a>
+    <a href="#">Speed Test</a>
+    <a href="#">Legal Notices</a>
+    <br>
+    Only on Netflix
+    <br>
+    <a href="#">English</a>
+    <br>
+    Netflix India
+  </footer>
+                        
+                    </div>
 
                 </div>
+
                </div>
             </div>
             
@@ -189,9 +288,6 @@ export default{
 .border2 {
     border: 1px solid #000000 !important
 }
-.thrd_div_main{
-
-}
 
 .thrd_div{
     margin-left: -125px;
@@ -209,9 +305,6 @@ export default{
     padding-left: 825px;
 
 }
-.dwnld_head{
-
-}
 .subhead-2{
     padding-left: 825px;
     color: white;
@@ -226,4 +319,199 @@ export default{
     height: 390px;
 
 }
+.fourth_div{
+    margin-top: 10px;
+    margin-left: -125px;
+    background-color: black;
+    height: 600px;
+    width: 1520px;
+}
+.watchevr{
+    font-weight: 800;
+    font-size: xxx-large;
+    color: white;
+    padding-top: 195px;
+    padding-left: 250px;
+}
+.subhead-3{
+    font-weight: 600;
+    font-size: x-large;
+    color: white;
+    padding-left: 255px;
+}
+.all-inone{
+    margin-top: -228px;
+    margin-left: 830px;
+    width: 540px;
+}
+.fifth_div{
+    margin-left: -125px;
+    background-color: black;
+    height: 835px;
+    width: 1520px;
+    margin-top: 15px;
+}
+.forkids{
+    font-weight: 800;
+    font-size: xxx-large;
+    color: white;
+    padding-top: 240px;
+    padding-left: 745px;
+}
+.subhead-4{
+    font-weight: 600;
+    font-size: x-large;
+    color: white;
+    padding-left: 750px;
+}
+.children{
+    margin-left: 100px;
+    height: 380px;
+    margin-top: -300px;
+}
+.questions{
+    font-weight: 900;
+    font-size: xxx-large;
+    color: white;
+    padding-top: 40px;
+    padding-left: 420px;
+}
+.ques-1{
+    margin-left: 170px;
+    height: 75px;
+    background-color: #474c4c;
+    width: 1180px;
+}
+.subhead-5{
+    font-weight: 600;
+    font-size: x-large;
+    padding-top: 23px;
+    color: white;
+    padding-left: 57px;
+}
+.ques-2{
+    margin-left: 45px;
+    margin-top: -645px;
+    background-color: #474c4c;
+    height: 75px;
+    width: 1180px;
+}
+.subhead-6{
+    font-weight: 600;
+    font-size: x-large;
+    padding-top: 23px;
+    color: white;
+    padding-left: 57px;
+}
+.ques-3{
+    margin-left: 45px;
+    margin-top: 10px;
+    background-color: #474c4c;
+    height: 75px;
+    width: 1180px;
+}
+.subhead-7{
+    font-weight: 600;
+    font-size: x-large;
+    padding-top: 23px;
+    color: white;
+    padding-left: 57px;
+}
+.ques-4{
+    margin-left: 45px;
+    margin-top: 10px;
+    background-color: #474c4c;
+    height: 75px;
+    width: 1180px;
+}
+.subhead-8{
+    font-weight: 600;
+    font-size: x-large;
+    padding-top: 23px;
+    color: white;
+    padding-left: 57px;
+}
+.ques-5{
+    margin-left: 45px;
+    margin-top: 10px;
+    background-color: #474c4c;
+    height: 75px;
+    width: 1180px;
+}
+.subhead-9{
+    font-weight: 600;
+    font-size: x-large;
+    padding-top: 23px;
+    color: white;
+    padding-left: 57px;
+}
+.ques-6{
+    margin-left: 45px;
+    margin-top: 10px;
+    background-color: #474c4c;
+    height: 75px;
+    width: 1180px;
+}
+.subhead-10{
+    font-weight: 600;
+    font-size: x-large;
+    padding-top: 23px;
+    color: white;
+    padding-left: 57px;
+}
+.getstarted{
+
+}
+.forh3{
+    padding-top: 50px;
+    padding-left: 231px;
+    width: 980px;
+    color: white;
+}
+.email2{
+
+}
+.getaddress{
+    background-color: #51574659;
+    margin-left: 263px;
+    width: 445px;
+    height: 54px;
+    border-radius: 3px;
+}
+.getbtn{
+    font-size: large;
+    color: white;
+    background-color: red;
+    border-radius: 6px;
+    margin-left: 715px;
+    height: 52px;
+    width: 190px;
+}
+.btnget{
+    margin-top: -52px;
+}
+.footer_div{
+    margin-top: 100px;
+    margin-left: -125px;
+    background-color: black;
+    height: 215px;
+    width: 1520px;
+}
+.footer {
+      background-color: #050000;
+      color: #000000;
+      padding: 20px 0;
+      text-align: center;
+      font-size: 14px;
+    }
+
+    .footer a {
+      color: #000000;
+      text-decoration: none;
+      margin: 0 10px;
+    }
+
+    .footer a:hover {
+      color: #e50914;
+    }
 </style>
